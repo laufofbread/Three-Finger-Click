a112f2299bbd260f461d6a644e082e2c541a17e7
Laura McCartney <hello@lauramccartney.co.uk>
2013-04-25 21:41:15 +0100

    Fix menu value on page open.

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
     
-    $("#e1").select2({maximumSelectionSize: 1});
+    $("#e1").val("");
+
+    $("#e1").select2({
+        maximumSelectionSize: 1,
+        placeholder: "Search for an article",
+    });
+
     
     $("#e1").on("change", function(e) {
-     
+        
      window.location = e.val[0]
      
     });
